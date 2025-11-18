ba3432a750233c8f897c846b7ae70c78da264b27
AKOLTINOBRIDGET <2300900107@mubs.ac.ug>
2025-11-18 15:35:12 +0300

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // ✅ enables class-based dark mode (important!)
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0, transform: "scale(0.95)" },
-          "100%": { opacity: 1, transform: "scale(1)" },
-        },
-      },
-      animation: {
-        "fade-in": "fadeIn 0.3s ease-out",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
